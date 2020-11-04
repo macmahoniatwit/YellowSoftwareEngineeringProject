46d4cf570e53c417247002bbc2a06f0d20356256
Vilarj <lamaravillasan@outlook.com>
2020-11-03 18:17:46 -0800

    Testing

--- driver.py
+++ driver.py
@@ -4,11 +4,12 @@ driver class of the program.
 '''
 
 from tkinter import *
-
+g
 # Root
 root = Tk()
 # Label of the window (widget)
 title = Label(root, text='Software Engineering Project')
 title.pack()
 
+# This creates a GUI window
 root.mainloop()
