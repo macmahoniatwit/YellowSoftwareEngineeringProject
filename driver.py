bf80a3d54552014497fb238e873d445435933dd4
Vilarj <lamaravillasan@outlook.com>
2020-11-03 18:52:15 -0800

    Playing around

--- driver.py
+++ driver.py
@@ -1,15 +1,24 @@
 '''
-This is intended to be the
-driver class of the program.
+This is intended to be the driver class of
+the program.
 '''
 
 from tkinter import *
-g
-# Root
-root = Tk()
-# Label of the window (widget)
-title = Label(root, text='Software Engineering Project')
-title.pack()
 
-# This creates a GUI window
-root.mainloop()
+class driver:
+    # Root
+    root = Tk()
+
+    # Label of the window (widget)
+    title = Label(root, text='Software Engineering Project')
+    title.grid(row=50,column=0)
+
+    # Buttons
+    button = Button(root, text='X', padx=25, command=click)
+    button.grid()
+
+    def click():
+        label2 = Label(root, text='Testing')
+        label2.pack()
+    # This creates a GUI window
+    root.mainloop()
