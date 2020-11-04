ddea2f8c1ff53afbc10732f5f190bb6fa8ae8304
Vilarj <lamaravillasan@outlook.com>
2020-11-03 18:14:19 -0800

    INIT Commit

---
+++ driver.py
@@ -0,0 +1,14 @@
+'''
+This is intended to be the
+driver class of the program.
+'''
+
+from tkinter import *
+
+# Root
+root = Tk()
+# Label of the window (widget)
+title = Label(root, text='Software Engineering Project')
+title.pack()
+
+root.mainloop()
