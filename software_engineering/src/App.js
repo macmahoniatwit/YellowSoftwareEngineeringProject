b462049ceb7f7bf9f72aedc4ec6c353858ff5c0d
Vilarj <lamaravillasan@outlook.com>
2020-11-05 11:40:19 -0800

    Some coloring to the page

--- software_engineering/src/App.js
+++ software_engineering/src/App.js
@@ -10,18 +10,26 @@ function App() {
       <header id="header"><b>Software Engineering Project</b>
         <nav>
           <ul class="Nav">
-            <li><a href="#" target="_self">Add a Session</a></li>
-            <li><a href="#" target="_self">Add a Speaker</a></li>
-            <li><a href="#" target="_self">Add a Room</a></li>
+            <li><a href="#" target="_self"><em>Add a Session</em></a></li>
+            <li><a href="#" target="_self"><em>Add a Speaker</em></a></li>
+            <li><a href="#" target="_self"><em>Add a Room</em></a></li>
           </ul>
         </nav>
       </header>
       <div class="App_Body">
         <body>
-        <div id="h2_1"><h2> Hello </h2></div>
-          <div id="p1">
-          <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
-          </div>
+          <div id="h2_1"><h2> Hello </h2></div>
+            <div id="p1">
+              <p>
+              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam,
+              feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
+              Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+              Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum,
+              eros ipsum rutrum orci,    sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.
+              Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
+              Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
+              </p>
+            </div>
         </body>
       </div>
     </div>
