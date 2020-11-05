ede15347eb715f49b822658a929e44c59a515f80
Vilarj <lamaravillasan@outlook.com>
2020-11-05 11:07:06 -0800

    Some coloring added to the page.

--- software_engineering/src/App.js
+++ software_engineering/src/App.js
@@ -7,11 +7,23 @@ import './components/Speaker.js';
 function App() {
   return (
     <div className="App">
-      <header><b>Software Engineering</b>
-        <ul><a href="" target="_blank"><div><em>Add a Session</em></div></a></ul>
-        <ul><a href="" target="_blank"><div><em>Add a Speaker</em></div></a></ul>
-        <ul><a href="" target="_blank"><div><em>Add a Room</em></div></a></ul>
+      <header id="header"><b>Software Engineering Project</b>
+        <nav>
+          <ul class="Nav">
+            <li><a href="#" target="_self">Add a Session</a></li>
+            <li><a href="#" target="_self">Add a Speaker</a></li>
+            <li><a href="#" target="_self">Add a Room</a></li>
+          </ul>
+        </nav>
       </header>
+      <div class="App_Body">
+        <body>
+        <div id="h2_1"><h2> Hello </h2></div>
+          <div id="p1">
+          <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+          </div>
+        </body>
+      </div>
     </div>
   );
 }
