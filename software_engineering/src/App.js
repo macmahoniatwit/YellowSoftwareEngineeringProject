34cce1ec48d991ef16ad173fb32fd126edbbb029
Vilarj <lamaravillasan@outlook.com>
2020-11-07 18:33:49 -0800

    asas

--- software_engineering/src/App.js
+++ software_engineering/src/App.js
@@ -6,9 +6,8 @@ function App() {
       <header id="header"><b>Software Engineering Project</b>
         <nav>
           <ul class="Nav">
+            <li><a href="#" target="_self"><em>Session List</em></a></li>
             <li><a href="#" target="_self"><em>Add a Session</em></a></li>
-            <li><a href="#" target="_self"><em>Add a Speaker</em></a></li>
-            <li><a href="#" target="_self"><em>Add a Room</em></a></li>
           </ul>
         </nav>
       </header>
